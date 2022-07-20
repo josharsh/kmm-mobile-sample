a449e7e23f5c6a81b1114d23707663205ab638ad
josharsh <harsh.joshi.pth@gmail.com>
2022-07-20 15:30:38 +0530

    Add code for starter coding exercise

--- shared/src/commonMain/kotlin/com/jetbrains/kmm/shared/Calculator.kt
+++ shared/src/commonMain/kotlin/com/jetbrains/kmm/shared/Calculator.kt
@@ -1,7 +1,5 @@
 package com.jetbrains.kmm.shared
 
 class Calculator {
-    companion object {
-        fun sum(a: Int, b: Int): Int = a + b
-    }
+
 }
--- shared/src/commonMain/kotlin/com/jetbrains/kmm/shared/Greeting.kt
+++ shared/src/commonMain/kotlin/com/jetbrains/kmm/shared/Greeting.kt
@@ -2,6 +2,6 @@ package com.jetbrains.kmm.shared
 
 class Greeting {
     fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
+
     }
 }
